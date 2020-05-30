69c6d24f3b772204c2044804e4c7ce8e10e6495e
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-30 00:16:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,41 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+
+def won?(board)
+
+  i = 0
+  j = 0
+  array = []
+  winner = []
+  WIN_COMBINATIONS.each do |look|
+        #win_index = win_combination[i]
+        #puts win_index
+        check = WIN_COMBINATIONS[i]
+       # puts check
+        position_1 = board[WIN_COMBINATIONS[i][0]]
+        position_2 = board[WIN_COMBINATIONS[i][1]]
+        position_3 = board[WIN_COMBINATIONS[i][2]]
+        array << [position_1, position_2, position_3]
+        #puts position_1, position_2, position_3
+        #puts "this: #{array}"
+
+        i += 1
+
+  end
+
+
+  while j < WIN_COMBINATIONS.length do
+    if  array[j][0] == "X" && array[j][1] == "X" && array[j][2] == "X"
+        winner = [WIN_COMBINATIONS[j][0], WIN_COMBINATIONS[j][1], WIN_COMBINATIONS[j][2]]
+        puts winner
+    elsif array[j][0] == "O" && array[j][1] == "O" && array[j][2] == "O"
+        winner = [WIN_COMBINATIONS[j][0], WIN_COMBINATIONS[j][1], WIN_COMBINATIONS[j][2]]
+        puts winner
+    else
+        nil
+    end
+    j += 1
+end
+
+end
